ca374a5a7d33918d21feb737b2f295d1d795e4f1
nileshtochia <nilesh.tochia@gmail.com>
2021-02-17 12:57:30 +0000

    Adds separate config for es5 and esmodule support

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+const es5Config = {
     entry: ['regenerator-runtime/runtime', './WaitForIt.js'],
     module: {
         rules: [
@@ -7,12 +7,50 @@ module.exports = {
                 exclude: /(node_modules)/,
                 use: {
                     loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', {
+                                targets: {
+                                    ie: '11'
+                                },
+                            }],
+                        ],
+                    }
                 }
             }
         ]
     },
     target: ['es5'],
     output: {
-        filename: 'WaitForIt.js',
+        filename: 'WaitForIt.es5.min.js',
     }
 }
+
+const config = {
+    entry: ['./WaitForIt.js'],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', {
+                                targets: {
+                                    "esmodules": true
+                                }
+                            }],
+                        ],
+                    }
+                }
+            }
+        ]
+    },
+    output: {
+        filename: 'WaitForIt.min.js',
+    }
+}
+
+module.exports = [es5Config, config];
